e9123b50e46673ca4c7afbfce81df549b5288ff8
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-10-01 20:50:04 -0300

    Uptades on ledaAutoUltraCommit

--- ledaAutoUltraCommit.sh
+++ ledaAutoUltraCommit.sh
@@ -2,42 +2,41 @@
 #Author: joaobb
 
 #Variables
-weekDay=$(date +%u);                     #Day of the week 1...7 (1 == Monday)
+weekDay=$(date +%u);                            #Day of the week 1...7 (1 == Monday)
 
-if [ -e "lastRoteiro.data" ];
-    then read input < lastRoteiro.data  #Reads the file to get the last roteiro sent
-else
-    read -p "Last roteiro digit (RXX-01): " input;
+if [ -e "lastRoteiro.data" ];                   #Verifies if lastRoteiro.data exists
+    then                                        #Reads lastRoteiro.data data and assigns to variables
+    read -r roteiroId matricula < lastRoteiro.data
+else                                            #Receaves last roteiroId and students matricula
+    read -p "Last roteiro digit (RXX-01): " roteiroId;
+    read -p "Your matricula (XXXXXXXXX): " matricula;
 fi
-
-matricula='117210327';                  #Student ID
 #/Variables
 
-if [ $weekDay -eq 1 ];                  #Today == Monday
-    then classTime=16 ;                 #Class time is 1600
-    ((input++))                         #Sets to a new roteiro
-elif [ $weekDay -eq 4 ];                #Today == Wednesday
-    then classTime=14 ;                 #Class time is 1400
-    ((input++))                         #Sets to a new roteiro
-else                                    #No LEDA class today
+if [ $weekDay -eq 1 ];                          #Today == Monday
+    then classTime=16 ;                         #Class time is 1600
+elif [ $weekDay -eq 4 ];                        #Today == Wednesday
+    then classTime=14 ;                         #Class time is 1400
+else                                            #No LEDA class today
     echo "There is no LEDA class today ;D";
     exit 1;    
 fi;
 
-
-if [ $input -lt 10 ];                   #String formatation
-    then roteiroName="R0"$input"-01";   #09
-else roteiroName="R"$input"-01";        #10
+((roteiroId++))                                 #Sets to a new roteiro
+if [ $roteiroId -lt 10 ];                       #String formatation
+    then roteiroName="R0"$roteiroId"-01";       #09
+else roteiroName="R"$roteiroId"-01";            #10
 fi
-echo $input > lastRoteiro.data          #Saves current roteiros number
+
+echo $roteiroId $matricula > lastRoteiro.data   #Storages the new roteiro id and your matricula
 
 while
-    currentHour=$(date +"%H");          #Updates system hour
-    currentTime=$(date +"%T");          #Updates system time
-    sleep 1;                            #Sleeps for 1 sec
-    clear;                              #Clear the terminal
+    currentHour=$(date +"%H");                  #Updates system hour
+    currentTime=$(date +"%T");                  #Updates system time
+    sleep 1;                                    #Sleeps for 1 sec
+    clear;                                      #Clear the terminal
     printf "\b\b$currentTime";
-    (( $currentHour < $classTime ))     #Verify if it is time to exit the loop
+    (( $currentHour < $classTime ))             #Verify if it is time to exit the loop
     do :; done
 
 #This part was taken from gustavolbs's LEDA-AUTO-SEND repository
