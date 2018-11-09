bf013ac38e375262615a8c768d6909086db1bc27
joaobb <joaopedro.bb98@gmail.com>
2018-11-09 15:27:50 -0300

    AAAAAA

--- facebook.sh
+++ facebook.sh
@@ -1,2 +1,3 @@
 x="https://m.facebook.com/story.php?story_fbid=445506072643884&id=201490800316184&_rdr"
-curl $x | grep "<meta property="og:video" content=" -c
+curl $x | grep -E "^<meta property=\"og:video\" content=\"Message\">.*/>$"
+echo $Message
