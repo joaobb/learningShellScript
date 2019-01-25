3a5cda45d7082e5c1af812fb7c61c9a9c22d19a0
joaobb <joaopedro.bb98@gmail.com>
2019-01-25 10:53:51 -0300

    Atualização no script de leda

--- ledaAutoUltraCommit.sh
+++ ledaAutoUltraCommit.sh
@@ -11,15 +11,14 @@ function user() {
     userSubmit=1
     read -p "Do you want to use gitHub? [y/n]: " gitUse;
     if [ "$gitUse" != "${gitUse#[YySs1]}" ] ;then
-        git pull
         git config --global credential.helper store
+        git pull
         gitUse=1;
-
     else gitUse=0
     fi
 
     if [ -e "lastRoteiro.data" ]; then                              #Verifies if it is the first time submitting for yourself
-                                                                    #Reads lastRoteiro.data data and assigns data to variables
+                                                            #Reads lastRoteiro.data data and assigns data to variables
         read -r matricula turma < lastRoteiro.data
     else                                                             #Gets alunos matricula, roteiroName and turma as input
        dataInput matricula, turma;
@@ -40,43 +39,42 @@ function getData() {
         #Gambi para que numeros como 08 nao sejam reconhecidos como octal
         serverSec=${serverHour:6:2}
         secUntilCommit=$(( (${commitHour:0:2} - ${serverHour:0:2}) * 3600 + (${commitHour:3:2} - ${serverHour:3:2}) * 60 + ${serverSec#0} ))
-        #Time until the commit should begin
-        timeUntillCommit=$(date -d@$secUntilCommit -u +%H:%M:%S)
-        echo $commitHour
-        echo $roteiroName
     else
         echo "There is no LEDA class today ;D";
         exit 1
     fi
-
     echo $serverDate
 }
 
 function dataInput() {
     read -p "Matricula to be submitted: (XXXXXXXXX): " matricula;   #Gets for matricula
     read -p "Aluno's turma: (X) " turma;                            #Gets turma of aluno
+	if [ $turma > 3 -o $turma < 1 -o $matricula < 100000000 -o $matricula > 999999999] ; then
+		echo "Wrong input, please try again"
+		exit 1;
+	fi
 }
 
 function gitHubCommit() {
     echo "Commiting to gitHub ..."
     git add .
-    git commit -m "Adição do $roteiroName"
+    git commit -m "Adição do "$roteiroName
     git push
 }
+
+#Here it starts
 
 getData
 
 read -p "Is this submit for you? [y/n]: " userSubmit;
 
-case "$userSubmit" in                                               #Checks if roteiro will be submitted for the user or another matricula
-["yY"] | ["sS"] | [1]) user matricula, roteiroName;;                  #Commit will be made with the data saved or will recieve it
-[nN] | [0]) dataInput matricula, roteiroName;                         #Commit will be made with a new input, and no aluno's data will be stored
-            userSubmit=0;;
+case "$userSubmit" in                                              #Checks if roteiro will be submitted for the user or another matricula
+["yY"] | ["sS"] | [1]) user matricula, turma;;               #Commit will be made with the data saved or will recieve it
+[nN] | [0]) 		   dataInput matricula, turma;           #Commit will be made with a new input, and no aluno's data will be stored
+            	       userSubmit=0;;
 *) echo "Wrong input, please try again";                            #Wrong input
    exit 1;;
 esac
-
-roteiroName=$roteiroName"-0"$turma
 
 # while [ $secUntilCommit != -1 ];
 #   do
@@ -85,6 +83,8 @@ roteiroName=$roteiroName"-0"$turma
 #     echo "Time until commit "$(date -d@$secUntilCommit -u +%H:%M:%S);
 #     (( secUntilCommit-- ))
 #   done
+
+roteiroName=$roteiroName"-0"$turma
 
 #This part was taken from gustavolbs's LEDA-AUTO-SEND repository
 wget -O "$roteiroName.zip" --post-data="id=$roteiroName&matricula=$matricula" http://150.165.85.29:81/download
@@ -100,12 +100,11 @@ mvn install -DskipTests
 if [ -e $matricula"-send.log" ]; then                           #Success on submitting the roteiro
     if [ $userSubmit -eq 1 ]; then                              #Data will be stored
         cd ..
-        ((roteiroId++))                                         #Sets roteiroId to the next one
-        echo $roteiroId $matricula $turma > lastRoteiro.data    #Storages the new roteiro id and your matricula
+        echo $matricula $turma > lastRoteiro.data    #Storages the new roteiro id and your matricula
     fi
 else
     echo "Something strange happened. Deleting this folder..."  #Something happaned and a log wasnt saved
-    rm -rf $roteiroId"roteiro"                                  #It will delete the roteiro and quit the script
+    rm -rf $roteiroName                                  #It will delete the roteiro and quit the script
     exit 1;
 fi
 
